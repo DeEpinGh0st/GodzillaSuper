323af55f07ba98dc30700e81b0151b44869b55f3
Xaaaa-bip <Xaaaa-bip@users.noreply.github.com>
2026-08-10 10:02:00 +0800

    chore: RELEASE_TAG 3.1.4 -> 3.1.5

--- src/core/ApplicationContext.java
+++ src/core/ApplicationContext.java
@@ -233,7 +233,7 @@ public class ApplicationContext {
     public static final String VERSION = "4.15";
 
     /** GSL release tag, checked against GitHub latest for update notification. */
-    public static final String RELEASE_TAG = "3.1.4";
+    public static final String RELEASE_TAG = "3.1.5";
 
 
 
